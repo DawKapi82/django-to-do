fe242523d7cc72bd7368f8ae0a768f06e07b3aa3
DawKapi82 <dawid_kapi82@hotmail.com>
2023-06-09 11:33:54 +0000

    CRUD

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e_29kym1$t4msz=&70kj#p990ppg-0cyxtlq^dc-ton+y1rc*)'
+SECRET_KEY = '(django-insecure-e_29kym1$t4msz=&70kj#p990ppg-0cyxtlq^dc-ton+y1rc*)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-dawkapi82-djangotodo-20nzx0ipkhg.ws-eu99.gitpod.io']
+ALLOWED_HOSTS = ['8000-dawkapi82-djangotodo-20nzx0ipkhg.ws-us99.gitpod.io']
 
 
 # Application definition
